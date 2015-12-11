83c107191944ccfcd43f5a23fe81651cab6ec6d9
Shashank Murthy <ShashankMurthy@Shashanks-MacBook-Pro.local>
2015-12-11 01:47:12 -0600

    mmmkaaaahh

--- src/demo/Enemy.java
+++ src/demo/Enemy.java
@@ -5,42 +5,38 @@ import hw3.Sprite;
 
 public class Enemy extends Sprite{
 
-	private double x = 0;
-	private double y = 0;
 	private double givenDx = 0;
 	private double givenDy = 0;
-	
+	private double left = 0;
+	private double right= 0;
 
 	public Enemy(double x, double y, int width, int height, Renderer givenRenderer) {
 		super(x, y, width, height, givenRenderer);
-		
-		this.x = x;
-		this.y = y;
+ 
 	}
 	public void setDirection(double givenDx, double givenDy){
-		this.x = givenDx;
-		this.y = givenDy;
+		this.givenDx = givenDx;
+		this.givenDy = givenDy;
 
 
 	}
 	public double getDx() {
 		// TODO Auto-generated method stub
-		return this.x;
+		return this.givenDx;
 	}
 
 	public double getDy() {
 		// TODO Auto-generated method stub
-		return this.y;
+		return this.givenDy;
 	}
 	public void setBounds(double left, double right){
-		if(this.getX() > Double.POSITIVE_INFINITY || this.getX() < Double.NEGATIVE_INFINITY)
-			this.setPosition(left, right);
+		
 	}
 	public void setParent(Platform parent){
 		parent.addChild(this);
 		setBounds(parent.getXExact()+parent.getWidth(),parent.getYExact()+parent.getHeight());
-		this.x += parent.getDx();
-		this.y += parent.getDy();
+		this.givenDx = parent.getDx();
+		this.givenDy = parent.getDy();
 	}
 	public void update(){
 		super.update();
